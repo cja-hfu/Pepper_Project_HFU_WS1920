bc58446d88f9fc5e46f328b944e4d62920699acc
cja-hfu <56632172+cja-hfu@users.noreply.github.com>
2019-11-07 13:12:16 +0100

    small fixes

--- html/js/script.js
+++ html/js/script.js
@@ -2,71 +2,71 @@
 var session;
 
 try {
-  QiSession( function (s) {
-    console.log('connected!');
-    session = s;
-    // now that we are connected, we can use the buttons on the page
-    $('button').prop('disabled', false);
-    s.service('ALMemory').then(function (memory) {
-      memory.subscriber('TouchChanged').then(function (subscriber) {
-        subscriber.signal.connect(changeTitle);
-      });
+    QiSession(function (s) {
+        console.log('connected!');
+        session = s;
+        // now that we are connected, we can use the buttons on the page
+        $('button').prop('disabled', false);
+        s.service('ALMemory').then(function (memory) {
+            memory.subscriber('TouchChanged').then(function (subscriber) {
+                subscriber.signal.connect(changeTitle);
+            });
+        });
     });
-  });
 } catch (err) {
-  console.log("Error when initializing QiSession: " + err.message);
-  console.log("Make sure you load this page from the robots server.")
+    console.log("Error when initializing QiSession: " + err.message);
+    console.log("Make sure you load this page from the robots server.")
 }
 
 $(function () {
-  $('#say').click(sayHelloWorld);
+    $('#say').click(sayHelloWorld);
 });
 
 $(function () {
-  $('#help').click(help);
+    $('#help').click(help);
 });
 
 
 $(function () {
-  $('#areyousure').click(areyousure);
+    $('#areyousure').click(areyousure);
 });
 
 $(function () {
-  $('#somethingtodrink').click(somethingtodrink);
+    $('#somethingtodrink').click(somethingtodrink);
 });
 
 function changeTitle(data) {
-  $('h1').text('Message received!')
+    $('h1').text('Message received!')
 }
 
 function sayHelloWorld() {
-  session.service('ALTextToSpeech').then(function (tts) {
-    tts.say('Sehr gut!, bitte denken Sie daran immer genug zu trinken');
-  }, function (error) {
-    console.log(error);
-  })
-  }
- 
+    session.service('ALTextToSpeech').then(function (tts) {
+        tts.say('Sehr gut!, bitte denken Sie daran immer genug zu trinken');
+    }, function (error) {
+        console.log(error);
+    })
+}
+
 function somethingtodrink() {
-  session.service('ALTextToSpeech').then(function (tts) {
-    tts.say('Oh! Dann sollten Sie schnell etwas trinken!');
-  }, function (error) {
-    console.log(error);
-  })
-  }
-  
+    session.service('ALTextToSpeech').then(function (tts) {
+        tts.say('Oh! Dann sollten Sie schnell etwas trinken!');
+    }, function (error) {
+        console.log(error);
+    })
+}
+
 function help() {
-  session.service('ALTextToSpeech').then(function (tts) {
-    tts.say('Wählen Sie ihre Antwort aus, in dem Sie die entsprechende Auswahl auf dem Bildschirm berühren!');
-  }, function (error) {
-    console.log(error);
-  })
-  }
-  
-  function areyousure() {
-  session.service('ALTextToSpeech').then(function (tts) {
-    tts.say('Sind Sie sich sicher?');
-  }, function (error) {
-    console.log(error);
-  })
+    session.service('ALTextToSpeech').then(function (tts) {
+        tts.say('Wählen Sie ihre Antwort aus, in dem Sie die entsprechende Auswahl auf dem Bildschirm berühren!');
+    }, function (error) {
+        console.log(error);
+    })
+}
+
+function areyousure() {
+    session.service('ALTextToSpeech').then(function (tts) {
+        tts.say('Sind Sie sich sicher?');
+    }, function (error) {
+        console.log(error);
+    })
 }
